9e5bc85a8652500ba478d5b056d50df3893ba2e6
dabeann <dabin4078@daum.net>
2024-03-28 19:28:30 +0900

    week3 주식을 사고팔기 가장 좋은 시점 (12)

--- src/week3/주식을_사고팔기_가장_좋은_시점_12.java
+++ src/week3/주식을_사고팔기_가장_좋은_시점_12.java
@@ -1,2 +1,24 @@
-package week3;public class 주식을_사고팔기_가장_좋은_시점_12 {
+package week3;
+
+import java.util.*;
+
+public class 주식을_사고팔기_가장_좋은_시점_12 {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length <= 1) {
+            return 0;
+        }
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+
+        return maxProfit;
+    }
 }
