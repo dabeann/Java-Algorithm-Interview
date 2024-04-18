d7ce49e0a1aeca7b708ce975fa4ff5abe6fef4f3
dabeann <dabin4078@daum.net>
2024-04-18 15:45:54 +0900

    week6 일일 온도 (22)

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -27,7 +27,7 @@ Y
 ,src/week5/역순_연결_리스트_II_19.java,a\f\af72021b445565c623608aa676a22afffdb6cce6
 R
 "src/week6/유효한_괄호_20.java,f\9\f9ef418c0d7702e065310427ab44004944c3ff43
-S
-#src/week6/중복_문자_제거.java,f\2\f274de287564f7d8cd27a6b6fd43d7105729fd5c
 V
 &src/week6/중복_문자_제거_21.java,4\2\42ce19a4bdbb055747a86cce9d50e2e3f21fecf9
+O
+src/week6/일일_온도_22.java,4\4\4498f5951ea583822ac9cc14ec2b8d869ff5e41e
---
+++ src/week6/일일_온도_22.java
@@ -0,0 +1,21 @@
+package week6;
+
+import java.util.*;
+
+public class 일일_온도_22 {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] answer = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int idx = stack.pop();
+                answer[idx] = i - idx;
+            }
+            stack.push(i);
+        }
+
+        return answer;
+    }
+}
