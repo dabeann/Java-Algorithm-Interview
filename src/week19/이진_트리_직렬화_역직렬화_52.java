da1147c0595f783b1a1541b9b9371ccc91de94df
dabeann <dabin4078@daum.net>
2024-08-06 14:39:06 +0900

    ✨  Feature : week19 이진 트리 직렬화 & 역직렬화 (52)

---
+++ src/week19/이진_트리_직렬화_역직렬화_52.java
@@ -0,0 +1,45 @@
+package week19;
+
+import java.util.*;
+
+public class 이진_트리_직렬화_역직렬화_52 {
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        serializeHelper(root, sb);
+        return sb.toString();
+    }
+
+    private void serializeHelper(TreeNode node, StringBuilder sb) {
+        if (node == null) {
+            sb.append("null").append(",");
+        } else {
+            sb.append(node.val).append(",");
+            serializeHelper(node.left, sb);
+            serializeHelper(node.right, sb);
+        }
+    }
+
+    public TreeNode deserialize(String data) {
+        Queue<String> queue = new LinkedList<>(Arrays.asList(data.split(",")));
+        return deserializeHelper(queue);
+    }
+
+    private TreeNode deserializeHelper(Queue<String> queue) {
+        String val = queue.poll();
+        if (val.equals("null")) {
+            return null;
+        }
+
+        TreeNode node = new TreeNode(Integer.parseInt(val));
+        node.left = deserializeHelper(queue);
+        node.right = deserializeHelper(queue);
+        return node;
+    }
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+}
