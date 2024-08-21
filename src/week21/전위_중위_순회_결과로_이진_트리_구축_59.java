98a298c0120002a272a6b7cc80d0aaaf4150752b
dabeann <dabin4078@daum.net>
2024-08-21 23:11:50 +0900

    ✨  Feature : week21 전위, 중위 순회 결과로 이진 트리 구축 (59)

---
+++ src/week21/전위_중위_순회_결과로_이진_트리_구축_59.java
@@ -0,0 +1,40 @@
+package week21;
+
+public class 전위_중위_순회_결과로_이진_트리_구축_59 {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return buildTreeHelper(preorder, inorder, 0, 0, inorder.length - 1);
+    }
+
+    private TreeNode buildTreeHelper(int[] preorder, int[] inorder, int preStart, int inStart, int inEnd) {
+        if (preStart > preorder.length - 1 || inStart > inEnd) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(preorder[preStart]);
+
+        int inIndex = 0;
+        for (int i = inStart; i <= inEnd; i++) {
+            if (inorder[i] == root.val) {
+                inIndex = i;
+            }
+        }
+
+        root.left = buildTreeHelper(preorder, inorder, preStart + 1, inStart, inIndex - 1);
+        root.right = buildTreeHelper(preorder, inorder, preStart + inIndex - inStart + 1, inIndex + 1, inEnd);
+
+        return root;
+    }
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+}
