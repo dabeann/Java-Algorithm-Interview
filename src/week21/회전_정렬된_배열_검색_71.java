51beae1da823b046d39cb13fa2f04bb6e8624b65
dabeann <dabin4078@daum.net>
2024-08-26 22:30:07 +0900

    ✨  Feature : week21 회전 정렬된 배열 검색 (71)

---
+++ src/week21/회전_정렬된_배열_검색_71.java
@@ -0,0 +1,32 @@
+package week21;
+
+public class 회전_정렬된_배열_검색_71 {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
