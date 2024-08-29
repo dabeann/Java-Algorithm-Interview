a14544c99364a3a151087e9526a442292a2dd3af
dabeann <dabin4078@daum.net>
2024-08-29 13:17:44 +0900

    ✨  Feature : week21 두 수의 합 II (73)

---
+++ src/week22/두_수의_합_II_73.java
@@ -0,0 +1,23 @@
+package week22;
+
+import java.util.*;
+
+public class 두_수의_합_II_73 {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+}
