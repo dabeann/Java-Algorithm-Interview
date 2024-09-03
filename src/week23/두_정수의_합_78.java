8a609c403051d6cfe7d5d76c5a105014be0f9631
dabeann <dabin4078@daum.net>
2024-09-03 15:07:50 +0900

    ✨  Feature : week23 두 정수의 합 (78)

---
+++ src/week23/두_정수의_합_78.java
@@ -0,0 +1,12 @@
+package week23;
+
+public class 두_정수의_합_78 {
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int carry = (a & b) << 1;
+            a = a ^ b;
+            b = carry;
+        }
+        return a;
+    }
+}
