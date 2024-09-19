af50573d595abf5be741294d9e1545c6178154fb
dabeann <dabin4078@daum.net>
2024-09-19 15:16:11 +0900

    ✨  Feature : week25 쿠키 부여 (88)

---
+++ src/week25/쿠키_부여_88.java
@@ -0,0 +1,21 @@
+package week25;
+
+import java.util.*;
+
+public class 쿠키_부여_88 {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int i = 0;
+        int j = 0;
+
+        while (i < g.length && j < s.length) {
+            if (s[j] >= g[i]) {
+                i++;
+            }
+            j++;
+        }
+        return i;
+    }
+}
