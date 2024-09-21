a202f71360648db86a6e71d8be5a9b81eed92faf
dabeann <dabin4078@daum.net>
2024-09-21 15:48:39 +0900

    ✨  Feature : week25 과반수 엘리먼트 (89) 풀이 추가

--- src/week25/과반수_엘리먼트_89.java
+++ src/week25/과반수_엘리먼트_89.java
@@ -9,6 +9,7 @@ public class 과반수_엘리먼트_89 {
         return nums[nums.length / 2];
     }
 
+    // 시간 초과 코드
     /*public int majorityElement(int[] nums) {
         Arrays.sort(nums);
 
@@ -19,5 +20,43 @@ public class 과반수_엘리먼트_89 {
             }
         }
         return -1;
+    }*/
+
+    // 엘리먼트 개수 계산
+    /*public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        List<Map.Entry<Integer, Integer>> entryList = new LinkedList<>(map.entrySet());
+        entryList.sort(Map.Entry.<Integer, Integer>comparingByValue().reversed());
+
+        return entryList.get(0).getKey();
+    }*/
+
+    // 분할 정복
+    /*public int majorityElement(int[] nums) {
+        return majorityElement(0, nums.length - 1, nums);
+    }
+
+    public int majorityElement(int left, int right, int[] nums) {
+        if (left == right) {
+            return nums[left];
+        }
+
+        int mid = left + (right - left) / 2;
+        int a = majorityElement(left, mid, nums);
+        int b = majorityElement(mid + 1, right, nums);
+
+        int countA = 0;
+        // 구간의 a 개수 계산
+        for (int i = left; i <= right; i++) {
+            if (nums[i] == a) {
+                countA++;
+            }
+        }
+        return countA > (right - left + 1) / 2 ? a : b;
     }*/
 }
