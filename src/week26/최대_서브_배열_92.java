69b8f5d69b54d4477a8ecdb13ad0973acae6f570
dabeann <dabin4078@daum.net>
2024-09-24 15:20:33 +0900

    ✨  Feature : week26 최대 서브 배열 (92)

---
+++ src/week26/최대_서브_배열_92.java
@@ -0,0 +1,14 @@
+package week26;
+
+public class 최대_서브_배열_92 {
+    public int maxSubArray(int[] nums) {
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        return maxSum;
+    }
+}
